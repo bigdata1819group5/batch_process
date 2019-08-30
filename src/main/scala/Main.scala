d79abe2a53415deb2824c7652a3cc86bb8948e26
Hamidreza Montaseri <hr.montaseri@gandom.co>
2019-08-30 22:33:09 +0430

    Use text file format

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,9 +8,10 @@ object Main {
 
     val context = Factory.createContext()
     val hdfsMaster = Properties.envOrElse("BATCH_HADOOP_NAMENODE", "hdfs://namenode:8020")
-    val data = context.hadoopFile(hdfsMaster + "/vehiclelocation/*", classOf[KeyValueTextInputFormat], classOf[Text], classOf[Text], 2)
-    val serialized = data.mapValues(v => Vehicle.create(v.toString))
-    val count = serialized.countByKey()
+    val data = context.textFile(hdfsMaster + "/user/spark/vehiclelocation/*", 2)
+    val serialized = data.map(value => Vehicle.create(value.toString))
+    val paired = serialized.map(v => (v.id, v))
+    val count = paired.countByKey()
     count.foreach(println(_))
     context.stop()
   }
